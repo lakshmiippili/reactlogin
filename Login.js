e6f6ab2a292c85e2239427395f5b9bc672c729ca
lakshmiippili <116436471+lakshmiippili@users.noreply.github.com>
2023-05-29 13:06:39 +0530

    useContext single context

--- Login.js
+++ Login.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useReducer } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import { useContext } from "react";
+import AuthContext from "../../store/authcontext";
 
 const emailReducer = (state, action) => {
   if (action.type === "user_input") {
@@ -66,6 +68,8 @@ const Login = (props) => {
     };
   }, [emailState, passwordState, collegeState]); //dependencies
 
+  const authCtx = useContext(AuthContext);
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "user_input", val: event.target.value });
   };
@@ -92,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value, collegeState);
+    authCtx.onLogin(emailState.value, passwordState.value, collegeState);
   };
 
   return (
